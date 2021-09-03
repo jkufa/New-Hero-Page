1d4a109ecb3f597ee0ee61ae7d1d86c735c5c007
Jack Kufa <jack@kufa.io>
2021-09-03 17:33:27 -0500

    Refactor background to work on infinite marquee loop

--- hero-page/src/components/background/Background.tsx
+++ hero-page/src/components/background/Background.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import styles from './Background.module.scss';
 
 const ROWS_SIZE = 20
-const COLS_SIZE = 14;
+const COLS_SIZE = 15;
 
 interface Props {
   bgText: string;
@@ -10,18 +10,24 @@ interface Props {
 
 const Background: FC<Props> = (Props) => {
   // Build bg text
-  const rows = Array(ROWS_SIZE).fill(<span>{ Array(COLS_SIZE).fill(Props.bgText + " ") }</span>);
   let i:number = 0;
+  const rows = Array(ROWS_SIZE).fill(Array(COLS_SIZE).fill(Props.bgText + " "));
   return (
     <div className={styles.bg}>
-      <div className={styles.bg_texts}>
-        {
-          rows.map((cols) => {
-            i = 1 - i;
-            return <h1 className={i ? styles.pos : styles.neg}>{ cols }</h1>
-          })
-        }
-      </div>
+      {
+        rows.map((cols) => {
+          i = 1 - i;
+          return (
+          <div className={`${styles.marquee_container}`}>
+            <h1 className={`${styles.marquee}`}>
+              <span>{ cols }</span> 
+            </h1>
+            <h1 className={`${styles.marquee} ${styles.marquee2}`}>
+              <span>{ cols }</span> 
+            </h1>
+          </div>)
+        })
+      }
   </div>
   );
 }
