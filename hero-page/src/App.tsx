e5a868f816cb14944596dfc9574ad6682dfb82b6
Jack Kufa <jack@kufa.io>
2021-09-05 22:18:02 -0500

    Add name to hero page and write really bad css **TODO: REFACTOR THIS!!**

--- hero-page/src/App.tsx
+++ hero-page/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Background from './components/background/Background';
 // import Button from './components/button/Button';
 import './styles/App.scss';
+import barline from './images/barline.svg';
 
 function App() {
   return (
     <div className="App">
       <div className="name-container">
-        <h1 className="lastName">KUFA</h1>
         <h1 className="firstName">JACK</h1>
+        <h1 className="lastName">KUFA</h1>
       </div>
       <div className="bg-container">
        <Background bgText={'jackkufa'} ></Background>
