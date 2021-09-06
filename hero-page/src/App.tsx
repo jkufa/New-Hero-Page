07337a351d32da956b05db3d218e57867c96648a
Jack Kufa <jack@kufa.io>
2021-09-05 19:34:17 -0500

    Refactor scss architecture & add text to App

--- hero-page/src/App.tsx
+++ hero-page/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Background from './components/background/Background';
-import Button from './components/button/Button';
+// import Button from './components/button/Button';
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Background bgText={'jackkufa'} ></Background>
+      <div className="name-container">
+        <h1 className="lastName">KUFA</h1>
+        <h1 className="firstName">JACK</h1>
+      </div>
+      <div className="bg-container">
+       <Background bgText={'jackkufa'} ></Background>
+      </div>
     </div>
   );
 }
