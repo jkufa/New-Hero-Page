34e7ef56ca2e5222a24d2eb8475a980cb298ec26
Jack Kufa <jack@kufa.io>
2021-09-07 11:19:40 -0500

    Add states for animations

--- hero-page/src/App.tsx
+++ hero-page/src/App.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Background from './components/background/Background';
 // import Button from './components/button/Button';
 import './styles/App.scss';
-import barline from './images/barline.svg';
 
 function App() {
+  const [fadingIn, setFadeIn] = useState<boolean>(true);
+  const [bouncingFirstName, setFirstNameBounce] = useState<boolean>(false);
+  const [bouncingLastName, setLastNameBounce] = useState<boolean>(false);
+
   return (
     <div className="App">
-      <div className="name-container">
-        <h1 className="firstName">JACK</h1>
-        <h1 className="lastName">KUFA</h1>
+      <div className="name-container"
+           onAnimationEnd={() => setFadeIn(() => false)}
+      >
+        <h1 
+          onMouseEnter={() => setFirstNameBounce(() => true)}
+          onAnimationEnd={() => setFirstNameBounce(() => false)}
+          className={`
+                     firstName 
+                     ${bouncingFirstName ? 'bouncing' : ''}
+                     ${fadingIn ? 'fadeIn' : ''}
+                    `}
+        >
+          JACK
+        </h1>
+        <h1
+          onMouseEnter={() => setLastNameBounce(() => true)}
+          onAnimationEnd={() => setLastNameBounce(() => false)} 
+          className={`
+                     lastName 
+                     ${bouncingLastName ? 'bouncing' : ''}
+                     ${fadingIn ? 'fadeIn' : ''}
+                    `}
+        >
+          KUFA
+        </h1>
       </div>
       <div className="bg-container">
        <Background bgText={'jackkufa'} ></Background>
